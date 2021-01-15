7c63686a1b1c1a2b956694d1ea954d072317aa88
Martin Weltler <1810640050@fh-burgenland.at>
2021-01-15 14:39:28 +0100

    Projektdateien hinzufügen.

---
+++ 001 T2K0 Student/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001_T2K0_Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
