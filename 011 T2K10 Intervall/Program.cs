9ce9053359d002676f26d9b8b86d7f27cb504a4f
Martin Weltler <1810640050@fh-burgenland.at>
2021-01-28 10:25:34 +0100

    T2K10 hinzugefügt

---
+++ 011 T2K10 Intervall/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _011_T2K10_Intervall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo 2x");
+        }
+    }
+}
